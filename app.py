308b5dd4bc3093839f2f47b6fe57abb483112bc3
GitHubDragonFly <69928501+GitHubDragonFly@users.noreply.github.com>
2021-03-15 16:51:25 -0400

    BUG fix and misc updates
    - BUG fix, use semicolon instead of comma for multiple tag separation so multi-dimensional arrays could be included.
    - Allow displaying each of the pages in a table. It requires adding the "templates" folder and the "TableList.html" file to the project. This feature is individually controlled by the "returnTable" boolean variable.
    - Allow showing links in each of the functions, which has no effect if the table option is selected. This feature is individually controlled by the "includeLinks" boolean variable.
    - Allow showing bit/bool values as either True : False or 1 : 0. This feature is only present in the get_tag function and is controlled by the "showBoolAsOneZero" boolean variable.
    - Allow reading elements of a single or multi dimensional array when included in array of tags (this is using multiple consecutive reads). This applies to reading bits of a single tag as well.
    - Add some error checking in all functions for when not connected.
    
    Example addressing:
    
    Single tag:
      -  localhost:5000/pylogix/v1.0/plc/192.168.0.10/0/tags/CT_DINT
      -  localhost:5000/pylogix/v1.0/plc/192.168.0.10/0/tags/CT_DINT.3
      -  localhost:5000/pylogix/v1.0/plc/192.168.0.10/0/tags/CT_DINT.5{12}
      -  localhost:5000/pylogix/v1.0/plc/192.168.0.10/0/tags/someArrayTag[x]{a}
      -  localhost:5000/pylogix/v1.0/plc/192.168.0.10/0/tags/someArrayTag[x, y]{a}
      -  localhost:5000/pylogix/v1.0/plc/192.168.0.10/0/tags/someArrayTag[x,y,z]{a}
    
    Multi tag:
      - localhost:5000/pylogix/v1.0/plc/192.168.1.15/3/tags/[CT_STRING; CT_REAL; CT_DINT; CT_DINT.0{7}; CT_BOOLArray[5]{25}; CT_DINT_3D[0,1,0]{3}]

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #!flask/bin/python
-from flask import Flask, jsonify
+from flask import Flask, jsonify, url_for, render_template
 from pylogix import PLC
 
 app = Flask(__name__)
@@ -7,62 +7,121 @@ app = Flask(__name__)
 @app.route('/pylogix/v1.0/plc/<ipAddress>/<int:slot>/tags', methods=['GET'])
 def get_all_tags(ipAddress, slot):
     tags = []
+    returnTable = False
+    includeLinks = False
+
     comm = PLC(ipAddress, slot)
     ret = comm.GetTagList()
     comm.Close()
+
+    if ret.Value is None:
+        return jsonify(ret.Status)
 
     for tag in ret.Value:
         tags.append(
             {"tagName"    : tag.TagName,
              "dataType"   : tag.DataType
             })
-        
-    return jsonify({'tags': tags})
+
+    if returnTable:
+        return render_template('TableList.html', values=tags, colnames=['tagName', 'dataType'])
+    else:
+        if includeLinks:
+            return jsonify({'tags': tags}, '------------------------------------------', 'Links To Other Pages', 'Discover Devices: ' + url_for('get_devices', ipAddress=ipAddress, slot=slot, _external=True))
+        else:
+            return jsonify({'tags': tags})
 
 @app.route('/pylogix/v1.0/plc/<ipAddress>/<int:slot>/tags/<tag>', methods=['GET'])
 def get_tag(tag, ipAddress, slot):
     comm = PLC(ipAddress, slot)
-    myTags = []
-    result = []
+    regularTags = []
+    arrayTags = dict()
+    results = []
     readArray = False
     arrayElementCount = 0
+    showBoolAsOneZero = False
+    returnTable = False
+    includeLinks = False
 
-    if tag.startswith('[') and tag.endswith(']'): # array of tags
-        tags = (tag[1:-1].replace(' ', '')).split(',')
+    if tag.startswith('[') and tag.endswith(']'): # array of mixed tags
+        tags = (tag[1:-1].replace(' ', '')).split(';')
+
         for t in tags:
             if not t == '':
-                myTags.append(t)
+                if t.endswith('}') and '{' in t: # 1 or 2 or 3 dimensional array tag
+                    try:
+                        arrayElementCount = int(t[t.index('{') + 1:t.index('}')])
+                        readArray = True
+                        t = t[:t.index('{')]
+                        arrayTags.update( {t : arrayElementCount} )
+                    except:
+                        pass
+                else:
+                    regularTags.append(t)
 
-        if len(myTags) > 0:
-            ret = comm.Read(myTags)
+        if len(regularTags) > 0:
+            ret = comm.Read(regularTags)
 
-            comm.Close()
+            if ret[0].Value is None:
+                comm.Close()
+                return jsonify(ret[0].Status)
 
             for i in range(0, len(ret)):
-                result.append(str(ret[i].TagName) + ' = ' + str(ret[i].Value))
-            
-            # TODO
-            # Need a full json response here, not just an array of values. 
-            # {
-            #     "tags": {
-            #         "tag": {
-            #             "status": "Success",
-            #             "tagName": "BaseBOOL",
-            #             "value": true
-            #         },
-            #         "tag": {
-            #             "status": "Success",
-            #             "tagName": "BaseDINT",
-            #             "value": 456464
-            #         }
-            #     }
-            # }
+                if showBoolAsOneZero and (str(ret[i].Value) == 'True' or str(ret[i].Value) == 'False'):
+                    results.append(
+                        {"tagName"  : str(ret[i].TagName),
+                         "value"    : 1 if str(ret[i].Value) == 'True' else 0,
+                         "status"   : str(ret[i].Status)
+                        })
+                else:
+                    results.append(
+                        {"tagName"  : str(ret[i].TagName),
+                         "value"    : str(ret[i].Value),
+                         "status"   : str(ret[i].Status)
+                        })
 
-            return jsonify(*result)
+        if len(arrayTags) > 0:
+            for tag in arrayTags:
+                ret = comm.Read(tag, arrayTags[tag])
+
+                if ret.Value is None:
+                    comm.Close()
+                    return jsonify(ret.Status)
+
+                if showBoolAsOneZero and (str(ret.Value[0]) == 'True' or str(ret.Value[0]) == 'False'):
+                    newBoolArray = []
+                    for val in range(0, len(ret.Value)):
+                        newBoolArray.append(1 if str(ret.Value[val]) == 'True' else 0)
+
+                    results.append(
+                        {"tagName"  : ret.TagName,
+                         "value"    : newBoolArray,
+                         "status"   : ret.Status
+                        })
+                else:
+                    results.append(
+                        {"tagName"  : ret.TagName,
+                         "value"    : ret.Value,
+                         "status"   : ret.Status
+                        })
+
+        comm.Close()
+
+        if len(regularTags) > 0 or len(arrayTags) > 0:
+            if returnTable:
+                return render_template('TableList.html', values=results, colnames=['tagName', 'value', 'status'])
+            else:
+                if includeLinks:
+                    return jsonify({'tags': results}, '------------------------------------------', 'Links To Other Pages', 'List Tags: ' + url_for('get_all_tags', ipAddress=ipAddress, slot=slot, _external=True), 'Discover Devices: ' + url_for('get_devices', ipAddress=ipAddress, slot=slot, _external=True))
+                else:
+                    return jsonify({'tags': results})
         else:
-            return jsonify('Not a valid tag!')
+            if includeLinks:
+                return jsonify('Not a valid tag!', '------------------------------------------', 'Links To Other Pages', 'List Tags: ' + url_for('get_all_tags', ipAddress=ipAddress, slot=slot, _external=True), 'Discover Devices: ' + url_for('get_devices', ipAddress=ipAddress, slot=slot, _external=True))
+            else:
+                return jsonify('Not a valid tag!')
     else:
-        if tag.endswith('}') and '{' in tag: # 1-dimensional array
+        if tag.endswith('}') and '{' in tag: # 1 or 2 or 3 dimensional array
             try:
                 arrayElementCount = int(tag[tag.index('{') + 1:tag.index('}')])
                 readArray = True
@@ -74,23 +133,69 @@ def get_tag(tag, ipAddress, slot):
 
         if readArray and arrayElementCount > 0:
             ret = comm.Read(tag, arrayElementCount)
+
+            if ret.Value is None:
+                comm.Close()
+                return jsonify(ret.Status)
+
+            if showBoolAsOneZero and (str(ret.Value[0]) == 'True' or str(ret.Value[0]) == 'False'):
+                newBoolArray = []
+                for val in range(0, len(ret.Value)):
+                    newBoolArray.append(1 if str(ret.Value[val]) == 'True' else 0)
+
+                results.append(
+                    {"tagName"  : ret.TagName,
+                     "value"    : newBoolArray,
+                     "status"   : ret.Status
+                    })
+            else:
+                results.append(
+                    {"tagName"    : ret.TagName,
+                     "value"      : ret.Value,
+                     "status"     : ret.Status
+                    })
         else:
             ret = comm.Read(tag)
 
+            if ret.Value is None:
+                comm.Close()
+                return jsonify(ret.Status)
+
+            if showBoolAsOneZero and (str(ret.Value) == 'True' or str(ret.Value) == 'False'):
+                results.append(
+                    {"tagName"  : ret.TagName,
+                     "value"    : 1 if str(ret.Value) == 'True' else 0,
+                     "status"   : ret.Status
+                    })
+            else:
+                results.append(
+                    {"tagName"    : ret.TagName,
+                     "value"      : ret.Value,
+                     "status"     : ret.Status
+                    })
+
         comm.Close()
 
-        return jsonify({'tag': {
-            'tagName'  : ret.TagName,
-            'value'    : ret.Value,
-            'status'   : ret.Status
-        }})
+        if returnTable:
+            return render_template('TableList.html', values=results, colnames=['tagName', 'value', 'status'])
+        else:
+            if includeLinks:
+                return jsonify({'tag': results}, '------------------------------------------', 'Links To Other Pages', 'List Tags: ' + url_for('get_all_tags', ipAddress=ipAddress, slot=slot, _external=True), 'Discover Devices: ' + url_for('get_devices', ipAddress=ipAddress, slot=slot, _external=True))
+            else:
+                return jsonify({'tag': results})
 
 @app.route('/pylogix/v1.0/plc/<ipAddress>/<int:slot>/devices', methods=['GET'])
 def get_devices(ipAddress, slot):
     devices = []
+    returnTable = False
+    includeLinks = False
+
     comm = PLC(ipAddress, slot)
     ret = comm.Discover()
     comm.Close()
+
+    if ret.Value == []:
+        return jsonify('No Devices Discovered')
 
     for device in ret.Value:
         devices.append(
@@ -98,7 +203,13 @@ def get_devices(ipAddress, slot):
              "revision"   : device.Revision
             })
         
-    return jsonify({'devices': devices})
+    if returnTable:
+        return render_template('TableList.html', values=devices, colnames=['productName', 'revision'])
+    else:
+        if includeLinks:
+            return jsonify({'devices': devices}, '------------------------------------------', 'Links To Other Pages', 'List Tags: ' + url_for('get_all_tags', ipAddress=ipAddress, slot=slot, _external=True))
+        else:
+            return jsonify({'devices': devices})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
